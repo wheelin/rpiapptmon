8edc1c0b743150163818fb054866d8dd0e32ecd7
wheelin <greg.emry@gmail.com>
2019-08-21 16:39:35 +0000

    connected to thingspeak and sending data

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,7 @@ use std::io::Write;
 use serde_json::json;
 use std::collections::HashMap;
 
-const ACCESS_TOKEN : &str = "Rbq9v5HG1b023o2rPcEZ";
-const ERR_CNTR_MAX : u8   = 10;
-const LOG_FILE     : &str = "/home/pi/log.txt";
-
-fn log_to_file<T : Into<String>>(msg : T) -> Result<(), std::io::Error> {
-    let now = Local::now();
-    let mut file = OpenOptions::new()
-                    .append(true)
-                    .create(true)
-                    .open(LOG_FILE)?;
-
-    writeln!(file, "{} [{}]", 
-        now.format("%d/%m/%Y-%H:%M:%S"),
-        msg.into(),
-    )?;
-    Ok(())
-}
-
+const ACCESS_TOKEN : &str = "NVSTS3TBBO8O4YEH";
 
 fn main() -> Result<(), std::io::Error> {
     let hum = HumiditySensor::new(
@@ -70,42 +53,19 @@ fn main() -> Result<(), std::io::Error> {
     loop {
         let light_val = light.get_all();
 
-        let data = json!({
-            "temperature" : bmp.read_temperature()?,
-            "pressure" : bmp.read_pressure(Oss::Oss4)?,
-            "humidity" : hum.get_humidity()?,
-            "light_clear" : light_val.0,
-            "light_red"   : light_val.1,
-            "light_green" : light_val.2,
-            "light_blue"  : light_val.3,
-            "airquality"  : airq.get_ratio_rs_r()?,
-        });
-        let request = format!("http://localhost:8080/api/v1/{}/telemetry", ACCESS_TOKEN);
-        match sndr.post(&request).json(&data).send() {
-            Ok(_) => {
-                cntr = 0;
-            },
-            Err(e) => {
-                cntr += 1;
-                log_to_file(format!("Error (counter = {}) while sending data : {:?}", cntr, e))?;
-                if cntr > ERR_CNTR_MAX {
-                    log_to_file(format!("Error : max (={}) reached", ERR_CNTR_MAX))?;
-                    panic!("Too many errors while attempting to send data to cloud");
-                };
-            },
-        }
-        log_to_file("Data sent with succes to the cloud.")?;
-
-        log_to_file(format!("temperature : {} *C", bmp.read_temperature()?))?;
-        log_to_file(format!("pressure    : {} Pa", bmp.read_pressure(Oss::Oss4)?))?;
-        log_to_file(format!("humidity    : {} %" , hum.get_humidity()?))?;
-        log_to_file(format!("airquality  : {}"   , airq.get_ratio_rs_r()?))?;
-        log_to_file(format!("light_clear : {}"   , light_val.0))?;
-        log_to_file(format!("light_red   : {}"   , light_val.1))?;
-        log_to_file(format!("light_green : {}"   , light_val.2))?;
-        log_to_file(format!("light_blue  : {}"   , light_val.3))?;
-
-        thread::sleep(Duration::new(30, 0));
+        let request = format!("https://api.thingspeak.com/update?api_key={}&field1={}&field2={}&field3={}&field4={}&field5={}&field6={}&field7={}&field8={}", 
+            ACCESS_TOKEN,
+            bmp.read_temperature()?,
+            bmp.read_pressure(Oss::Oss4)?,
+            hum.get_humidity()?,
+            airq.get_ratio_rs_r()?,
+            light_val.0,
+            light_val.1,
+            light_val.2,
+            light_val.3,
+        );
+        sndr.get(&request).send().unwrap();
+        thread::sleep(Duration::new(60, 0));
     }
     
     Ok(())
