3795e4e76ae9c84ae61e2c1be77695d1e67a33a4
wheelin <greg.emry@gmail.com>
2019-09-03 18:56:58 +0200

    enable on event information sending for pir sensor

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,16 @@ use sensors::press_temp::*;
 use std::thread;
 use std::time::Duration;
 
-const ACCESS_TOKEN: &str = "NVSTS3TBBO8O4YEH";
+const ACCESS_TOKEN: &str = include_str!("../thingspeak_token.txt");
+// curl -H "Authorization: Bearer eyJrIjoiS0gyOUpLN3hqbzVQYkpNcHVSTUswbmJnUWQ0MmZQWnkiLCJuIjoiZW1nIiwiaWQiOjF9" https://wheelin.grafana.net/api/dashboards/home
 
 fn when_detected() {
-    println!("Detected");
+    let sndr = Client::new();
+    let request = format!("https://api.thingspeak.com/update?api_key={}&field6={}",
+        ACCESS_TOKEN,
+        1,
+    );
+    sndr.get(&request).send().unwrap();
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -36,21 +42,16 @@ fn main() -> Result<(), std::io::Error> {
     let sndr = Client::new();
 
     loop {
-        //let request = format!("https://api.thingspeak.com/update?api_key={}&field1={}&field2={}&field3={}&field4={}&field5={}&field6={}&field7={}&field8={}",
-        //    ACCESS_TOKEN,
-        //    bmp.read_temperature()?,
-        //    bmp.read_pressure(Oss::Oss4)?,
-        //    hum.get_humidity()?,
-        //    airq.get_ratio_rs_r()?,
-        //    light_val.0,
-        //    light_val.1,
-        //    light_val.2,
-        //    light_val.3,
-        //);
-        //sndr.get(&request).send().unwrap();
-        //thread::sleep(Duration::new(60, 0));
-        println!("light : {}", pr.get_ratio()?);
-        thread::sleep(Duration::from_secs(1));
+        let request = format!("https://api.thingspeak.com/update?api_key={}&field1={}&field2={}&field3={}&field4={}&field5={}",
+            ACCESS_TOKEN,
+            bmp.read_temperature()?,
+            bmp.read_pressure(Oss::Oss4)?,
+            hum.get_humidity()?,
+            airq.get_ratio_rs_r()?,
+            pr.get_ratio()?,
+        );
+        sndr.get(&request).send().unwrap();
+        thread::sleep(Duration::new(60, 0));
     }
     Ok(())
 }
